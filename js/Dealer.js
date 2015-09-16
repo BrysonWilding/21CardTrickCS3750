95d1204f04605f3131f6ff0b960409f424c2473f
ferrinsp <ferrinsp@gmail.com>
2015-09-16 13:21:08 -0600

    Change Deck Class

--- js/Dealer.js
+++ js/Dealer.js
@@ -3,3 +3,14 @@
 //deal()
 //revealCard()
 //pickupCards()
+
+int row = 0, col = 0, card = 0;
+for (row = 0; row < 7; row++)
+{
+    play[row][col] = deck[card];
+    for (col = 0; col < 3; col++)
+    {
+        play[row][col] = deck[card];
+        card++;
+    } 
+}
--- js/Deck.js
+++ js/Deck.js
@@ -1,2 +1,56 @@
-﻿//shuffle()
-//random21()
+﻿var Deck = function () {
+    var cardArray = [];
+    var playCards = [7][3];
+    var publicVar = this;
+
+    //initializes a brand new deck and shuffles it
+    function initCards() {
+        /*initialize cards and their types*/
+        cardArray = [];
+        for (var i = 0; i < 4; i++) {
+            var type = "";
+            switch (i) {
+                case 0:
+                    type = "heart";
+                    break;
+                case 1:
+                    type = "diamond";
+                    break;
+                case 2:
+                    type = "spade";
+                    break;
+                default:
+                    type = "club";
+            }
+            for (var j = 1; j <= 13; j++) {
+                cardArray.push(new Card({ suit: type, value: j }))
+            }
+        }
+        /*end init*/
+
+        //shuffle new deck
+        publicVar.shuffle();
+    }
+
+    //shuffles the card array
+    publicVar.shuffle = function () {
+        var o = cardArray;
+        for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+    };
+
+    //outputs a listing of all the cards in the current deck. helper function for debugging
+    publicVar.showCards = function () {
+        return cardArray;
+    };
+
+    //checks to see if 10 or less cards in the deck, if so, we just shuffle a new deck
+    publicVar.checkCardCount = function () {
+        if (cardArray.length <= 21) {
+            initCards();
+        }
+    };
+
+    //init new instance of deck
+    initCards();
+};
