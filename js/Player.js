051b64ac9d7b12befcc4eea83f40517c0a00b017
ferrinsp <ferrinsp@gmail.com>
2015-09-21 08:22:10 -0600

    Added selectedCard logic, need to work on column

--- js/Player.js
+++ js/Player.js
@@ -5,11 +5,16 @@
         // It depends on the user interface. Do we want a button below each column?
         // Or is it selected and highlighted?
         // The user interface defines how this will work
-
     };
     this.pickCard = function pickCard(){
         // I think this just asks the player to select a card
         // Give a message that says something like "Pick a card, memorize it, maybe write it down"
         // Then it could just say select a column after making your decision.
+
+        alert("Please pick a card by clicking on the card of choice.");
+        do {
+            alert("You have not selected a card, please select a card by clicking on the card of choice.");
+        } while(this.selectedCard == false);
+        alert("Thanks for selecting a card, please select the column button your card is in.");
     }
 };
