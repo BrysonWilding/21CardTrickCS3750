15e12aaea0c13cb2bae08f1d093ba1a24859dd96
Tatetaylor <tatewtaylor@yahoo.com>
2015-09-21 09:19:04 -0600

    Updated Player

--- js/Player.js
+++ js/Player.js
@@ -1,19 +1,26 @@
-﻿var Player = function(selectedCard){
-    this.selectedCard = selectedCard;
+﻿var Player = function(){
+
     this.indicateColumn = function indicateColumn(){
         // For user comprehension the columns will be selected as "Left", "Middle", or "Right".
         // It depends on the user interface. Do we want a button below each column?
         // Or is it selected and highlighted?
         // The user interface defines how this will work
-
+        //May need to play with variables and how they are passed
+        var selectedCard=false;
+        alert("Please select the column where your card is located.")
+        do {
+            alert("You have not selected a column, please pick a column to proceed.");
+        } while (selectedCard == false);
+        return this.indicateColumn;
     };
     this.pickCard = function pickCard(){
         // I think this just asks the player to select a card
         // Give a message that says something like "Pick a card, memorize it, maybe write it down"
         // Then it could just say select a column after making your decision.
-        alert("Please select the column your card is in.")
+        var selectedCard=false;
+        alert("Please select the column where your card is located.")
         do {
             alert("You have not selected a column, please pick a column to proceed.");
-        } while (this.selectedCard == false);
+        } while (selectedCard == false);
     }
 };
